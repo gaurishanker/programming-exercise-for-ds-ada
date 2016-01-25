ee1e670064238f77c93df50c12f612efed63546a
Gaurishanker <gaurishankargaur93@gmail.com>
2016-01-26 00:42:18 +0530

    modified treeset

--- collections/set/treesetexample.java
+++ collections/set/treesetexample.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class treesetexample{
 public static void main(String args[]){
-String words[]="mohit is pakiya     . jaggu is   laki luffy attack gomu    gomu no santoryu gomu gomu no diable jumbo".split("[. ]+");;
+String words[]="repeat word are different word is are repeat are".split("[. ]+");;
 TreeSet s=new TreeSet();
 for(String w:words)
 s.add(w);
