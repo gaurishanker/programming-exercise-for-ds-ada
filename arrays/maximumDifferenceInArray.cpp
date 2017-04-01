e8ae037fd66d55f86a3bd3550798a984aa0d6f80
gaurishanker <gaurishankargaur93@gmail.com>
2017-04-01 08:00:49 +0530

    maximum difference between two elements of array

--- arrays/maximumDifferenceInArray.cpp
+++ arrays/maximumDifferenceInArray.cpp
@@ -3,6 +3,7 @@
 // If array is [ 7, 9, 5, 6, 3, 2 ] then returned value should be 2 (Diff between 7 and 9)
 #include <iostream>
 using namespace std;
+
 int main()
  {
     int t;
@@ -15,7 +16,7 @@ int main()
         for(int i=0;i<n;++i)
             cin>>a[i];
         int min=a[0];
-        int max=-1;
+        int max = -1;
         for(int i=1;i<n;++i)
         {
             if(a[i]<min)
