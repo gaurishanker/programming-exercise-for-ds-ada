b6bec3f7b04f13c5878c70f0d6820f4e30e769c9
gaurishanker <gaurishankargaur93@gmail.com>
2017-04-25 23:04:58 +0530

    move elements to end in a linked list

--- linkedlist/moveAllOccurenceToEnd.c
+++ linkedlist/moveAllOccurenceToEnd.c
@@ -23,8 +23,9 @@ void push(struct node** head,int data)
 
 }
 
-void printList(struct node* node)
+void printList(struct node* curr)
 {
+	struct node* node = curr;
 	while(node != NULL)
 	{
 		printf("%d ",node->data);
@@ -32,13 +33,62 @@ void printList(struct node* node)
 	}
 }
 
+void move(struct node **head,int key)
+{
+	if(*head == NULL)
+		return;
+	struct node* prev=NULL;
+	struct node* curr=*head;
+	struct node* next=*head;
+	struct node* secondh = NULL;
+	int i=0;
+	while(curr->next != NULL)
+	{
+		++i;
+		next=curr->next;
+		if(curr == *head && curr->data==key)
+		{
+			if(secondh == NULL)
+				curr->next=NULL;
+			else
+			{
+				curr->next = secondh;
+				
+			}
+			secondh = curr;
+			*head = next;
+			curr= next;
+		}
+		else if( curr != *head && curr -> data == key )
+		{
+			prev->next = curr ->next;
+			curr->next = secondh;
+			secondh = curr;
+			curr = next;
+		}
+		else
+		{
+			prev = curr;
+			curr = curr -> next;
+		}
+		
+	}
+	curr->next = secondh;
+}
+
 int main(int argc, char const *argv[])
 {
 	struct node* head = NULL;
+	push(&head,5);
+	push(&head,2);
 	push(&head,3);
 	push(&head,2);
 	push(&head,1);
 	printf("Linked list is\n");
+	printList(head);
+
+	move(&head,2);
+	printf("\nAfter moving linked list is\n");
 	printList(head);
 	return 0;
 }
